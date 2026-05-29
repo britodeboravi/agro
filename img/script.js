c946721092ecc5201c0a50e114782762a57ccd82
britodeboravi <debora.brito.santos@escola.pr.gov.br>
2026-05-29 14:08:21 +0000

    1

---
+++ img/script.js
@@ -0,0 +1,9 @@
+function aprender(tema) {
+    const mensagens = {
+        'Ecologia': 'Iniciando aventura na floresta! Vamos proteger a biodiversidade? 🌳',
+        'Tecnologia': 'Conectando às APIs da Embrapa... A agricultura digital é o futuro! 💻',
+        'Solos': 'Mergulhando na terra! Descubra como os nutrientes alimentam o mundo. 🥕'
+    };
+    
+    alert(mensagens[tema] || "Bem-vindo ao portal de conhecimento!");
+}
